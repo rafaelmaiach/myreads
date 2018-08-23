d0ef986afdbd803dd74b0385d47efabb37024cf6
rafael.maia1 <rafael.maia1@ibm.com>
2018-08-22 23:19:59 -0300

    feat: Integrate Auth system and Protected Route to application
    - Create dummy components just for render test

---
+++ src/containers/SearchScreen.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SearchScreen = () => <div>Search Screen</div>;
+
+export default SearchScreen;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './containers/App';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+import { AuthProvider } from './components/control/AuthContext';
+import ProtectedRoute from './components/control/ProtectedRoute';
+
+import LoginScreen from './containers/LoginScreen';
+import BookshelfScreen from './containers/BookshelfScreen';
+import SearchScreen from './containers/SearchScreen';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.scss';
 
-ReactDOM.render(
+// #TODO: Create 404 page and change Redirect to redirect to it
+
+/**
+ * @constructor App
+ * @description Represents the application's main component.
+ * It renders the specific component for each route defined
+ */
+const App = () => (
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <AuthProvider>
+      <Switch>
+        <Route exact path="/" component={LoginScreen} />
+        <ProtectedRoute path="/bookshelf" component={BookshelfScreen} />
+        <ProtectedRoute path="/search" component={SearchScreen} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </AuthProvider>
+  </BrowserRouter>
+);
+
+ReactDOM.render(
+  <App />,
   document.getElementById('root'),
 );
 registerServiceWorker();
