fa2dc9ed316c5de7b4777ea8a16c504b60e0cf0d
rafael.maia1 <rafael.maia1@ibm.com>
2018-08-23 01:42:47 -0300

    feat: Create initial app page
    - First page created and styled
    - The find a book button is opening the elements that will be the form

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+const showForm = () => {
+  const el = document.querySelector('.login-container__form');
+  el.classList.add('active');
+};
+
 const LoginScreen = () => (
-  <div>
-    Login Screen!
+  <div className="login-container">
+    <div className="login-container__box">
+      <div className="login-container__box__logo">
+        <img src="./assets/icons/logo.svg" alt="myreads-logo" />
+        <span>MYREADS</span>
+      </div>
+      <div className="login-container__box__button">
+        <button type="button" onClick={showForm}>FIND A BOOK</button>
+      </div>
+    </div>
+    <div className="login-container__form">
+      <div>
+        <h2>
+          What state do you live in?
+        </h2>
+      </div>
+      <div>
+        <p>Denial. Miss Wormwood: I don't suppose I can argue with that.</p>
+      </div>
+    </div>
   </div>
 );
 
