4561d596c9a640da456e4ce0d2c2673ccfcf7369
rafael.maia1 <rafael.maia1@ibm.com>
2018-08-23 17:59:07 -0300

    styles: Add style to AuthorQuotes

--- src/components/login/AuthorQuotes.js
+++ src/components/login/AuthorQuotes.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const AuthorQuotes = () => <div>AuthorQuotes</div>;
+const AuthorQuotes = () => (
+  <Fragment>
+    <h1 className="quotes__box--text">
+      If you have other things in your life—family, friends, good productive day work—these can
+      interact with your writing and the sum will be all the richer.
+    </h1>
+    <h3 className="quotes__box--author">By: Author</h3>
+  </Fragment>
+);
 
 export default AuthorQuotes;
