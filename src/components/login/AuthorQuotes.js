59efd35b106a315ea82fb4c5e583a75937261706
rafael.maia1 <rafael.maia1@ibm.com>
2018-08-23 22:04:09 -0300

    feat: Create method to get random quotes
    - It's using pre defined quotes that were get on internet

--- src/components/login/AuthorQuotes.js
+++ src/components/login/AuthorQuotes.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react';
+import getRandomQuote from '../../utils/WritingQuotes';
 
-const AuthorQuotes = () => (
-  <Fragment>
-    <h1 className="quotes__box--text">
-      If you have other things in your life—family, friends, good productive day work—these can
-      interact with your writing and the sum will be all the richer.
-    </h1>
-    <h3 className="quotes__box--author">By: Author</h3>
-  </Fragment>
-);
+const AuthorQuotes = () => {
+  const { quote, author } = getRandomQuote();
+
+  return (
+    <Fragment>
+      <h1 className="quotes__box--text">
+        {quote}
+      </h1>
+      <h3 className="quotes__box--author">
+        {author}
+      </h3>
+    </Fragment>
+  );
+};
 
 export default AuthorQuotes;
